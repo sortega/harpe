0f2fbcbf7ceb950748228394a854be07b635d521
Sebastian Ortega <sortega@tid.es>
2012-06-10 21:53:56 +0200

    Replace reflection lookup by template method pattern
    Less magic but more explicit and language supported.
    Also higher test coverage.

---
+++ src/test/java/org/refeed/harpe/FloatingValidationTest.java
@@ -0,0 +1,26 @@
+package org.refeed.harpe;
+
+import org.junit.Test;
+
+import static org.refeed.harpe.IntegerValidation.between;
+
+public class FloatingValidationTest extends BaseValidatorTest {
+
+    @Test
+    public void testBetween() {
+        RuleCheck<Integer> instance = between(0, 10);
+        assertValid(instance.run(0));
+        assertValid(instance.run(5));
+        assertValid(instance.run(10));
+        assertHasError(instance.run(-1), "must be at least 0");
+        assertHasError(instance.run(11), "must be at most 10");
+    }
+
+    @Test
+    public void testAtLeast() {
+    }
+
+    @Test
+    public void testAtMost() {
+    }
+}
